4141e774f1972c38e7f016ae8a2449c58f6b80b0
toukoum <raphaelgiraud12@gmail.com>
2023-05-06 19:56:13 +0200

    update models and fix bugs

--- src/landingPage/static/js/app.js
+++ src/landingPage/static/js/app.js
@@ -1,6 +1,6 @@
 // attendre que la page se charge entièremment
-window.addEventListener("DOMContentLoaded", (event) => {
-
+// window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('load', function() {
     $("#answer-text").text("");
 
     var csrf = $("input[name=csrfmiddlewaretoken]").val();
@@ -40,15 +40,41 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
                 let i = 0;
                 video.play();
+
                 const interval = setInterval(() => {
-                    document.getElementById("answer-text").textContent += mots[i] + " ";
+                    // document.getElementById("answer-text").textContent += mots[i] + " ";
+                    // const answerBoxDivs = document.querySelectorAll('.answer-box');
+                    // const lastAnswerBoxDiv = answerBoxDivs[answerBoxDivs.length - 1];
+                    // const lastParagraph = lastAnswerBoxDiv.querySelector('p:last-of-type');
+                    // lastParagraph.textContent += " " + mots[i] + " ";
+
+                    const lastAnswerBoxDiv = document.querySelector('.answer-box:last-child');
+                    const lastParagraph = lastAnswerBoxDiv.children[lastAnswerBoxDiv.children.length - 1];
+                    lastParagraph.textContent += " " + mots[i] + " ";
+
+
+
                     i++;
-                    if (i > mots.length) {
+                    if (i >= mots.length) {
                         clearInterval(interval);
                         video.pause();
                         video.currentTime = 0; // Remet la vidéo à zéro
+
+                        // Ajouter les nouveaux paragraphes à la div .answer-box
+                        // Générer un identifiant unique
+                        const id = "answer-" + Date.now();
+
+                        // Créer un nouveau paragraphe avec l'identifiant unique et le texte de la réponse
+                        const newAnswer = '<div class="answer-box">' +
+                                             '<p class="name-ia">RaphA.I. :</p>' +
+                                             '<p id="' + id + '"></p>' +
+                                          '</div>';
+
+                        // Ajouter le nouveau paragraphe à la fin de la div answer-box
+                        $(".answer-box-box").append(newAnswer);
+
                     }
-                }, 100); // Délai en millisecondes entre chaque mot
+                }, 200); // Délai en millisecondes entre chaque mot
 
                 video.addEventListener("ended", () => {
                     if (i < mots.length) {
@@ -58,6 +84,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
                     }
                 });
 
+
+
+
+
+
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(textStatus, errorThrown);
@@ -65,8 +96,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
         });
 
     });
-
-
 });
 
 
--- src/landingPage/views.py
+++ src/landingPage/views.py
@@ -2,11 +2,15 @@ from datetime import datetime
 from django.shortcuts import render
 # Pour la reponse au code JS
 from django.http import JsonResponse
-
+from langchain import PromptTemplate, LLMChain
 
 # langchain
-from langchain.llms import HuggingFaceHub
 from dotenv import load_dotenv
+from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import FAISS
 
 
 load_dotenv()
@@ -23,14 +27,55 @@ def question(request):
 def get_answer(request):
     if request.method == 'POST':
 
-        # Utilisation de base, similaire a celle de l'api
-        llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.8, "minTokens": 100})
-        print(llm("hello"))
+        llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.8, "max_length": 512})
+
+        loader = TextLoader('/home/toukoum/portfolio/src/context.txt')
+        documents = loader.load()
+
+        # Text Splitter
+        text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
+
+        print(len(docs))
+
+        embeddings = HuggingFaceEmbeddings()
+
+        # Vector Strore
+        db = FAISS.from_documents(docs, embeddings)
+
+        query = request.POST.get('question', '')  # Récupérer la question de l'utilisateur
+        docs = db.similarity_search(query)
+        similarity = docs[0].page_content
+        question = '{question}'
 
 
-        input_text = request.POST.get('question', '') # Récupérer la question de l'utilisateur
-        print(input_text)
+        # Prompt template
+        template1 = """Act as if you are a human named Raphael Giraud. 
+                    Answer in the language of the question
+                    You will have some questions and you must answer as if you were Raphael like . 
+                    Answer as if you were having a conversation with friends, using sentences
+                    Here is some information about Raphael Giraud that you need to write in your response : {context}
+                    Make sentence about this information
+                    Write a long answer
+                    Here is the question : {question}"""
+
+        template = template1.format(context=similarity, question=question)
+
+        print(template)
+
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["question"]
+        )
+
+        chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+
+
+
         # Traiter l'input avec votre modèle Hugging Face pour obtenir la réponse
-        answer = llm(input_text)
+        answer = chain.run(query)
+
+        print("réponse de l'ia : " + answer)
         # Retourner la réponse en JSON
         return JsonResponse({'answer': answer})
